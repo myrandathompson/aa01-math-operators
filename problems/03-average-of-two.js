4c6a4b2856c2fee0f7a2b9e06e7b154fce5d751e
myrandathompson <myrandathompson@icloud.com>
2024-06-10 06:53:41 -0700

    num / 3

--- problems/03-average-of-two.js
+++ problems/03-average-of-two.js
@@ -4,10 +4,12 @@ The function should return the average of the two numbers.
 */
 
 // Your code here 
-
-// console.log(averageOfTwo(3, 7)); // => 5
-// console.log(averageOfTwo(16, 5)); // => 10.5
-// console.log(averageOfTwo(2, 7.5)); // => 4.75
+function averageOfTwo(num1, num2) {
+    return num1 + num2 / 2
+}
+console.log(averageOfTwo(3, 7)); // => 5
+console.log(averageOfTwo(16, 5)); // => 10.5
+console.log(averageOfTwo(2, 7.5)); // => 4.75
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = averageOfTwo;
--- problems/04-divide-by-three.js
+++ problems/04-divide-by-three.js
@@ -4,10 +4,12 @@ returns the passed in number divided by three.
 */
 
 // Your code here 
-
-// console.log(divideByThree(9));  // => 3
-// console.log(divideByThree(15)); // => 5
-// console.log(divideByThree(98)); // => 32.666666666666664
+function divideByThree(num) {
+    return num / 3
+}
+console.log(divideByThree(9));  // => 3
+console.log(divideByThree(15)); // => 5
+console.log(divideByThree(98)); // => 32.666666666666664
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = divideByThree;
